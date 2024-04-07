76384bd2098bf017ea1c17367c93553a61f60e54
mosi <mosshengqi@outlook.com>
2024-04-07 16:59:43 +0800

    feat: move DeleteIcon components

---
+++ packages/common/dist/components/DeleteIcon/index.d.ts
@@ -0,0 +1,7 @@
+import React from 'react';
+interface Props {
+    color?: string;
+}
+export declare const DeleteIcon: React.FC<Props>;
+export {};
+//# sourceMappingURL=index.d.ts.map
--- packages/common/dist/utils/hooks/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { useAge } from './useAge';
-//# sourceMappingURL=index.d.ts.map
