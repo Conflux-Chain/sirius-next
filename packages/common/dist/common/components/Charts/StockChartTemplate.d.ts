0b85942aa68a69e72d58d00f2310b567a1545a5e
mosi <mosshengqi@outlook.com>
2024-04-11 11:24:36 +0800

    feat: update dist

--- packages/common/dist/common/components/Charts/StockChartTemplate.d.ts
+++
@@ -1,3 +0,0 @@
-import { ChartsProps } from './config';
-export declare function StockChartTemplate({ options, request }: ChartsProps): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=StockChartTemplate.d.ts.map
--- packages/common/dist/locales/i18n.d.ts
+++
@@ -1,7 +0,0 @@
-declare const enMerge: any;
-export type TranslationResource = typeof enMerge;
-export type LanguageKey = keyof TranslationResource;
-export declare const translations: any;
-export declare const i18n: any;
-export {};
-//# sourceMappingURL=i18n.d.ts.map
