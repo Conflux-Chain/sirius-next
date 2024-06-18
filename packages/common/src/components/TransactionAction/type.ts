c110d5b218c4fc65d9b74d6934d2655b74d77f6e
cryptosunshine <shouhui.sun@gmail.com>
2024-06-18 17:11:11 +0800

    fix: rm type Separate core evm

--- packages/common/src/components/TransactionAction/type.ts
+++ packages/common/src/components/TransactionAction/type.ts
@@ -8,7 +8,6 @@ export interface TransactionActionProps {
   transaction: TranslationArgs;
   event: EventList[];
   customInfo: CustomInfo;
-  outputType: 'base32' | 'hex';
 }
 
 export interface TokenInfo {
