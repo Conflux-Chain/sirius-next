9c4218574e2f8fd74359db3026e108e57c06c792
mosi <mosshengqi@outlook.com>
2025-03-25 10:57:57 +0800

    feat: add Image

--- packages/common/src/components/ImageSkeleton/index.tsx
+++ packages/common/src/components/ImageSkeleton/index.tsx
@@ -3,29 +3,24 @@ import React from 'react';
 
 export const ImageSkeleton: React.FC = () => {
   return (
-    <div
-      className={clsx(
-        'ant-skeleton ant-skeleton-element',
-        'inline-block w-auto',
-      )}
-    >
+    <div className={clsx('skeleton skeleton-element', 'inline-block w-auto')}>
       <div
         className={clsx(
-          'ant-skeleton-image',
-          'absolute w-full h-full top-0 left-0 right-0 bottom-0 object-contain rounded-t-5px flex-center vertical-top lh-96px bg-#bebebe33',
+          'skeleton-image',
+          'flex-center vertical-top lh-96px bg-#bebebe33 w-[96px] h-[96px]',
         )}
       >
         <svg
           viewBox="0 0 1098 1024"
           xmlns="http://www.w3.org/2000/svg"
           className={clsx(
-            'ant-skeleton-image-svg',
+            'skeleton-image-svg',
             'w-48px h-48px lh-48px max-w-192px max-h-192px',
           )}
         >
           <path
             d="M365.714286 329.142857q0 45.714286-32.036571 77.677714t-77.677714 32.036571-77.677714-32.036571-32.036571-77.677714 32.036571-77.677714 77.677714-32.036571 77.677714 32.036571 32.036571 77.677714zM950.857143 548.571429l0 256-804.571429 0 0-109.714286 182.857143-182.857143 91.428571 91.428571 292.571429-292.571429zM1005.714286 146.285714l-914.285714 0q-7.460571 0-12.873143 5.412571t-5.412571 12.873143l0 694.857143q0 7.460571 5.412571 12.873143t12.873143 5.412571l914.285714 0q7.460571 0 12.873143-5.412571t5.412571-12.873143l0-694.857143q0-7.460571-5.412571-12.873143t-12.873143-5.412571zM1097.142857 164.571429l0 694.857143q0 37.741714-26.843429 64.585143t-64.585143 26.843429l-914.285714 0q-37.741714 0-64.585143-26.843429t-26.843429-64.585143l0-694.857143q0-37.741714 26.843429-64.585143t64.585143-26.843429l914.285714 0q37.741714 0 64.585143 26.843429t26.843429 64.585143z"
-            className={clsx('ant-skeleton-image-path', 'fill-#bfbfbf')}
+            className={clsx('skeleton-image-path', 'fill-#bfbfbf')}
           ></path>
         </svg>
       </div>
