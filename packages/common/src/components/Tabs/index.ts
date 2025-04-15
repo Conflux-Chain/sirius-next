50515a750c29125bf34ed899b5ee5aafd5845a69
mosi <mosshengqi@outlook.com>
2025-04-15 14:19:17 +0800

    feat: move Tabs

---
+++ packages/common/src/components/Tabs/index.ts
@@ -0,0 +1,9 @@
+import Tabs from './tabs';
+import TabsItem from './tabs-item';
+export * from './styled-tabs';
+export * from './sub-tabs';
+
+Tabs.Item = TabsItem;
+Tabs.Tab = TabsItem;
+
+export default Tabs;
