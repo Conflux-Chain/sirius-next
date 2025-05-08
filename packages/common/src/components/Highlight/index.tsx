e796fbe55029b005b74792431257a1df276422e8
mosi <mosshengqi@outlook.com>
2025-05-08 16:08:28 +0800

    optimize

--- packages/common/src/components/Highlight/index.tsx
+++ packages/common/src/components/Highlight/index.tsx
@@ -1,29 +1,36 @@
 import clsx from 'clsx';
+import { useCallback, useMemo } from 'react';
 import { useHighlightStore } from 'src/store';
 
-export const ValueHighlight: React.FC<{
-  value?: unknown;
-  scope: string;
-  children: React.ReactNode;
-  className?: string;
-  style?: React.CSSProperties;
-}> = ({ value, scope, children, className, style }) => {
+export const ValueHighlight: React.FC<
+  {
+    value?: unknown;
+    scope: string;
+  } & React.HTMLAttributes<HTMLDivElement>
+> = ({ value, scope, children, className, ...props }) => {
   const { highlight, setHighlight } = useHighlightStore();
-  const isHighlight =
-    value && highlight.scope === scope && highlight.value === value;
-  const handleMouseEnter = (e: React.MouseEvent<HTMLDivElement>) => {
-    const data = (e.target as any).dataset;
-    if (!value || data?.scope === 'tooltip') return;
-    setHighlight(scope, value);
-  };
+  const isHighlight = useMemo(
+    () => value && highlight.scope === scope && highlight.value === value,
+    [value, scope, highlight.value, highlight.scope],
+  );
+  const handleMouseEnter = useCallback(
+    (e: React.MouseEvent<HTMLDivElement>) => {
+      if (isHighlight) return;
+      const data = (e.target as any).dataset;
+      if (!value || data?.scope === 'tooltip') return;
+      setHighlight(scope, value);
+    },
+    [scope, value, isHighlight, setHighlight],
+  );
+  const handleMouseLeave = useCallback(() => {
+    if (!value) return;
+    setHighlight();
+  }, [value, setHighlight]);
   return (
     <div
       onMouseEnter={handleMouseEnter}
       onMouseOver={handleMouseEnter}
-      onMouseLeave={() => {
-        if (!value) return;
-        setHighlight();
-      }}
+      onMouseLeave={handleMouseLeave}
       className={clsx(
         'border-1px border-dashed w-fit px-12px rounded-12px',
         isHighlight
@@ -31,7 +38,7 @@ export const ValueHighlight: React.FC<{
           : 'border-transparent',
         className,
       )}
-      style={style}
+      {...props}
     >
       {children}
     </div>
