3e3d278d5074b346db69a486e81e94b11b1167c4
mosi <mosshengqi@outlook.com>
2026-01-28 09:50:29 +0800

    feat: add DecimalsSelect

---
+++ packages/common/src/components/Icons/index.tsx
@@ -0,0 +1,57 @@
+export const ArrowDown: React.FC<React.SVGProps<SVGSVGElement>> = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#clip0_7684_174)">
+      <rect width="16" height="16" rx="2" fill="#7789D3" fillOpacity="0.08" />
+      <path
+        d="M4 6L8 10L12 6"
+        stroke="#9B9EAC"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_7684_174">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export const Add: React.FC<React.SVGProps<SVGSVGElement>> = props => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    {...props}
+  >
+    <g clipPath="url(#clip0_7684_167)">
+      <rect width="16" height="16" rx="2" fill="#7789D3" fillOpacity="0.08" />
+      <path
+        d="M4 8H12"
+        stroke="#9B9EAC"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8 4L8 12"
+        stroke="#9B9EAC"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_7684_167">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+);
